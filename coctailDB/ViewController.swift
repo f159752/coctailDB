9842068ee0c55125bbed7a43f3cb1b46130daed2
Artem <f159752@gmail.com>
2020-03-27 12:57:00 +0200

    Initial Commit

---
+++ coctailDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coctailDB
+//
+//  Created by Artem Shpilka on 3/27/20.
+//  Copyright © 2020 Artem Shpilka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
